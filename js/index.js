e9cbd5683b71028dc4996e46b9191bb0f7bfa593
jcperez <jose.perez@infocura.be>
2025-07-31 00:16:20 +0200

    Animation and form added.

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 3000,       
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.headline', {
+  duration: 3000,       
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.skills', {
+  duration: 3000,       
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.hero',{
+    duration: 3000,       
+        origin: 'bottom',
+        distance: '-100px'
+    }); 
+sr.reveal('.about', {
+  duration: 6000,       
+    origin: 'bottom',
+    distance: '-100px',
+    interval: 200
+});
