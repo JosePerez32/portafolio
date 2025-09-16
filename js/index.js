b7641b880e458b3ce75f346c119415502e7b7951
jcperez <jose.perez@infocura.be>
2025-09-16 09:51:47 +0200

    Extra changes.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 window.sr = ScrollReveal();
 sr.reveal('.navbar', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px'
 });
 sr.reveal('.headline', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px'
 });
 sr.reveal('.skills', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px'
 });
 sr.reveal('.hero',{
-    duration: 3000,       
+    duration: 2500,       
         origin: 'bottom',
         distance: '-100px'
     }); 
@@ -26,61 +26,61 @@ sr.reveal('.about', {
     interval: 200
 });
 sr.reveal('.experience-section', { 
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px',
     interval: 200
 }); 
 sr.reveal('.portfolio-section', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px',
     interval: 200
 });
 sr.reveal('.contact-section', {
-  duration: 5000,       
+  duration: 2000,       
     origin: 'bottom',
     distance: '100px',
     interval: 200
 }); 
 sr.reveal('.academic-section', {
-  duration: 3000,    
+  duration: 2500,    
     origin: 'bottom',
     distance: '-100px',
     interval: 200       
 }); 
 sr.reveal('.social-icons', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px'
 });
 sr.reveal('.vertical-line', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px',
     interval: 200
 });
 sr.reveal('.about-title', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px',
     interval: 200
 });
 sr.reveal('.about-title::after', {
-  duration: 3000,       
+  duration: 2500,       
     origin: 'bottom',
     distance: '-100px',
     interval: 200
 });
 
 sr.reveal('.vertical-line.animate', {
-  duration: 3000,
+  duration: 2500,
     origin: 'bottom',
     distance: '-100px',
     interval: 200   
 });
 sr.reveal('.Languages', {
-  duration: 3000,    
+  duration: 2500,    
     origin: 'bottom',
     distance: '-100px',
     interval: 200   
